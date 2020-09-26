4869437bbe485f8fb1ca42f71515a3ac6bbb256b
Macjus-ML <abolshakovy@gmail.com>
2020-09-26 21:25:04 +0300

    Fixed comments from 1 CodeReview

--- api/models.py
+++ api/models.py
@@ -33,40 +33,45 @@ class User(AbstractUser):
 
 class Genre(models.Model):
     """
-    Жанры произведений. Одно произведение может быть привязано к нескольким жанрам.
+    Жанры произведений.
+    Одно произведение может быть привязано к нескольким жанрам.
     """
     name = models.CharField(max_length=200, verbose_name="Жанр", unique=True)
     slug = models.SlugField(max_length=50, unique=True)
 
-    def __str__(self):
-        return self.name
-
     class Meta:
         verbose_name = 'Жанр'
         verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        return self.name
 
 
 class Category(models.Model):
     """
     Категории (типы) произведений («Фильмы», «Книги», «Музыка»).
     """
-    name = models.CharField(max_length=200, verbose_name="Категория", unique=True)
+    name = models.CharField(
+        max_length=200, verbose_name="Категория", unique=True
+    )
     slug = models.SlugField(max_length=50, unique=True)
-
-    def __str__(self):
-        return self.name
 
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
 
+    def __str__(self):
+        return self.name
+
 
 class Title(models.Model):
     """
-    Произведения, к которым пишут отзывы (определённый фильм, книга или песенка).
+    Произведения, к которым пишут отзывы
+    (определённый фильм, книга или песенка).
     """
     name = models.CharField(max_length=200, verbose_name='Произведение')
-    year = models.IntegerField(null=True)
+    year = models.IntegerField(
+        null=True, verbose_name="Год издания", db_index=True)
     description = models.CharField(max_length=200, null=True)
     genre = models.ManyToManyField(Genre, blank=True, related_name="titles")
     category = models.ForeignKey(Category, on_delete=models.SET_NULL,
--- api/views.py
+++ api/views.py
@@ -15,7 +15,6 @@ from rest_framework.viewsets import GenericViewSet, ModelViewSet
 from rest_framework_simplejwt.tokens import RefreshToken
 
 from api.filters import TitleFilter
-
 from .models import Category, Genre, Review, Title, User
 from .permissions import (IsAdmin, IsAdminOrReadOnly, IsAuthor, IsModerator,
                           IsSuperuser)
@@ -55,7 +54,7 @@ class TokenView(APIView):
     """
     Вью, принимает email и confirmation_code, возвращает токен
     """
-    permission_classes = (AllowAny, )
+    permission_classes = (AllowAny,)
 
     def get_token(self, user):
         refresh = RefreshToken.for_user(user)
@@ -103,7 +102,8 @@ class TitlesViewSet(ModelViewSet):
     """
     Viewset который предоставляет CRUD-действия для произведений
     """
-    queryset = Title.objects.annotate(rating=Avg('reviews__score')).order_by('id')
+    queryset = Title.objects.annotate(
+        rating=Avg('reviews__score')).order_by('id')
     filter_backends = (DjangoFilterBackend, SearchFilter)
     permission_classes = [IsAuthenticatedOrReadOnly, IsAdminOrReadOnly]
     filterset_class = TitleFilter
